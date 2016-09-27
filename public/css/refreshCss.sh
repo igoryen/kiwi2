274c43cee3a8b2a31ecfdc2945025c9876452fe8
igoryen <igor.yentaltsev@gmail.com>
2016-09-27 00:29:46 -0400

    Added link to go back to igoryen.com

--- public/css/refreshCss.sh
+++ public/css/refreshCss.sh
@@ -1,5 +1,5 @@
 
-lessc color.less color.css
-lessc layout.less layout.css
-lessc text.less text.css
-lessc main.less main.css
+lessc color.less compiled/color.css
+lessc layout.less compiled/layout.css
+lessc text.less compiled/text.css
+lessc main.less compiled/main.css
